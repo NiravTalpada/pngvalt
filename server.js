2f1bd6e87e17a53424c4f2e84730795554131710
Nirav Talpada <46027363+NiravTalpada@users.noreply.github.com>
2023-06-24 07:33:14 +0100

    init

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/angular-starter')));
+app.use(express.static(path.join(__dirname, 'src/pngvalt')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/angular-starter', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src/pngvalt', 'index.html'));
 });
 
 const port = 4000;
